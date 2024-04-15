30af6fd507e0275b615332c631f2dd5c4d41b05d
chiadmi <chiadmi.ismail@gmail.com>
2024-04-15 11:29:12 +0200

    création du fichier jour_4 pour le travail sur les packages

---
+++ jour_4/testpackage.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+url="https://catalog.data.gov/dataset"
+
+response = requests.get(url)
+# print(response)
+content = response.content
+# print(content)
+
+# soup : c'est tout le code hmtl
+soup=BeautifulSoup(content, 'html.parser')
+
+# Trouver la balise img
+image_tags = soup.find_all('img')
+print(image_tags)
+
+# Obtenir l'URL de l'image
+# image_url = img["src"]
+
+# print("URL de l'image :", image_url)
+
+# Obtenir les URLs de toutes les images
+image_urls = [img["src"] for img in image_tags]
+
+# Afficher les URLs des images
+for url in image_urls:
+    print("URL de l'image :", url)
